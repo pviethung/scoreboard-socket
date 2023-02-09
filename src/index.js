85f0647a5d6a374b0aa64ccfab2edf8f08af9017
pviethung <pv2hung97@gmail.com>
2023-02-09 16:05:54 +0700

    add cors

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: '*'
+  }
+});
 
 
 app.get('/', (req, res) => {
@@ -14,6 +18,11 @@ io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
   });
+
+  socket.on('disconnect', () => {
+    io.emit('chat message', "someone has left the chat")
+  })
+
 });
 
 server.listen(3000, () => {
